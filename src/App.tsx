964aea7ec5031889df6527a9fd5478395b5476ec
Nandini Singh <singhnandini60810@gmail.com>
2026-07-27 16:55:44 +0530

    adding

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,71 @@
-import { Route, Routes } from 'react-router-dom'
-import AppLayout from './components/layout/AppLayout'
-import DashboardPage from './pages/DashboardPage'
-import ExpensesPage from './pages/ExpensesPage'
-import NotFoundPage from './pages/NotFoundPage'
-import ReportsPage from './pages/ReportsPage'
+import { Route, Routes } from "react-router-dom";
+
+import AppLayout from "./components/layout/AppLayout";
+
+import DashboardPage from "./pages/DashboardPage";
+import ExpensesPage from "./pages/ExpensesPage";
+import ReportsPage from "./pages/ReportsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+import { LoginPage } from "./pages/LoginPage";
+import { SignupPage } from "./pages/SignupPage";
+import { VerifyEmailPage } from "./pages/VerifyEmailPage";
+import { ForgotPasswordPage } from "./pages/ForgotPasswordPage";
+
+import { ProtectedRoute } from "./components/auth/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
-      <Route element={<AppLayout />}>
-        <Route path="/" element={<DashboardPage />} />
-        <Route path="/expenses" element={<ExpensesPage />} />
-        <Route path="/reports" element={<ReportsPage />} />
-        <Route path="*" element={<NotFoundPage />} />
+      {/* ---------- Public Routes ---------- */}
+
+      <Route path="/login" element={<LoginPage />} />
+
+      <Route path="/signup" element={<SignupPage />} />
+
+      <Route
+        path="/verify-email"
+        element={<VerifyEmailPage />}
+      />
+
+      <Route
+        path="/forgot-password"
+        element={<ForgotPasswordPage />}
+      />
+
+      {/* ---------- Protected Routes ---------- */}
+
+      <Route
+        element={
+          <ProtectedRoute>
+            <AppLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route
+          path="/"
+          element={<DashboardPage />}
+        />
+
+        <Route
+          path="/expenses"
+          element={<ExpensesPage />}
+        />
+
+        <Route
+          path="/reports"
+          element={<ReportsPage />}
+        />
       </Route>
+
+      {/* ---------- 404 ---------- */}
+
+      <Route
+        path="*"
+        element={<NotFoundPage />}
+      />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,21 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+
+import { AuthProvider } from "./context/AuthContext";
 import { ExpenseProvider } from "./context/ExpenseContext";
 
 import "./index.css";
 import "./styles/auth.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <ExpenseProvider>
-        <App />
-      </ExpenseProvider>
+      <AuthProvider>
+        <ExpenseProvider>
+          <App />
+        </ExpenseProvider>
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 );
